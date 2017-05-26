9fe7f99942749b2c892c1c84a14097f2ed31ce55
xyz30800 <xyz30800@gmail.com>
2017-05-26 19:53:30 +0800

    fixed the googlemap bug

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -1,28 +1,71 @@
 import React, {Component} from 'react';
 import { GoogleMapLoader, GoogleMap, Marker } from 'react-google-maps';
 
-const GoogleMapComponent = (props) => {
+class GoogleMapComponent extends Component {
 
-	const [ lat, lon ] = [ parseFloat(props.lat), parseFloat(props.lon) ]
+	constructor(props) {
+		super(props);
+		
+		this.state = {
+			location: {
+				lat: 0,
+				lng: 0
+			},
+			wait: true
+		};
+	}
+
+	componentWillMount() {
+		this.setState({
+			location: {
+				lat: parseFloat(this.props.lat),
+				lng: parseFloat(this.props.lon)
+			}		
+		});
+	}
+
+	componentWillReceiveProps(nextProps) {
+		// 先丟出'讀取中......'的畫面
+		this.setState({
+        	wait: false
+        });
+        // 再 re-render 地圖資料
+		setTimeout(() => {
+            this.setState({
+            	location: {
+					lat: parseFloat(nextProps.lat),
+					lng: parseFloat(nextProps.lon)
+				},
+            	wait: true
+            })
+        }, 100);
+	}
 	
-	return (
-		<GoogleMapLoader
-			containerElement={ <div style={{height: '100%'}} /> }
-			googleMapElement={
-				<GoogleMap
-					defaultZoom={15} 
-					defaultCenter={{lat: lat, lng: lon}} >
-	                <Marker 
-	                	icon=""
-	                	position={{
-					        lat: lat,
-					        lng: lon,
-					    }}
-	                />
-				</GoogleMap>
-			}
-		/>
-	);
+	render() {
+
+		let map = {};
+		if (!this.state.location.lat) {
+			map = <div className="invalid-map">沒有經緯度資料</div>;
+		} else if (!this.state.wait) {
+			map = <div className="invalid-map">讀取中......</div>;
+		} else {
+			map = (
+				<GoogleMapLoader
+					containerElement={ <div style={{height: '100%'}} /> }
+					googleMapElement={
+						<GoogleMap
+							defaultZoom={15} 
+							defaultCenter={this.state.location} >
+			                <Marker position={this.state.location} />
+						</GoogleMap>
+					}
+				>
+				</GoogleMapLoader>
+			);
+		}
+
+		return map;
+	}
 }
 
 export default GoogleMapComponent;
